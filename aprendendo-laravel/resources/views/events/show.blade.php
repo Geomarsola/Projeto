f9fde070cf0a04f666aa12628a2bec9a0faff7fa
Giovane Marsola <giovane.marsola@hotmail.com>
2025-09-11 20:17:41 -0300

    Aula 19

--- aprendendo-laravel/resources/views/events/show.blade.php
+++ aprendendo-laravel/resources/views/events/show.blade.php
@@ -15,6 +15,11 @@
         <p class="events participants"><ion-icon name="people-outline"></ion-icon> X Participantes</p>
         <p class="event-owner"><ion-icon name="star-outline"></ion-icon> Dono do evento</p>
         <a href="#" class="btn btn-primary" id="event-submit">Confirmar Presença</a>
+        <h3>O evento conta com:<h/3>
+        <ul id="items-list">
+          @foreach($event->items as $item)
+          <li><ion-icon name="play-outline"></ion-icon> <span>{{ $item }}</li>
+          @endforeach
       </div>
       <div class="col-md-12" id="description-contaniner">
         <h3>Sobre o evento:</h3>
