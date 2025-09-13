8220b6d3160ac52d23e1cfc0f330f045310b0862
Giovane Marsola <giovane.marsola@hotmail.com>
2025-09-12 22:33:31 -0300

    aula 21

--- aprendendo-laravel/resources/views/welcome.blade.php
+++ aprendendo-laravel/resources/views/welcome.blade.php
@@ -11,8 +11,12 @@
     </form>
 </div>
 <div id="events-container" class=col-md-12>
+    @if($search)
+    <h2>Buscando por: {{ $search }}</h2>
+    @else
     <h2>Proximos Eventos</h2>
     <p class="subtitle">Veja os eventos dos próximos dias</p>
+    @endif
     <div id="cards-container" class="row">
         @foreach($events as $event)
         <div class="card col-md3">
@@ -25,8 +29,10 @@
            </div>
         </div>
         @endforeach
-        @if(count($events) == 0)
-        <p>Não há eventos disponíveis</p>
+        @if(count($events) == 0 && $search)
+            <p>Não foi possível encontrar nenhum evento com {{ $search }}! <a href="/"> Ver todos!</p>
+        @elseif(count($events) == 0)
+            <p>Não há eventos disponíveis</p>
         @endif
 </div>
 </div>
