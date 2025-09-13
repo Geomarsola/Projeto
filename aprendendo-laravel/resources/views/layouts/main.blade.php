1ed891b24a24343c0874ba479652ab93e9c8ea26
Giovane Marsola <giovane.marsola@hotmail.com>
2025-09-13 20:33:48 -0300

    aula 22

--- aprendendo-laravel/resources/views/layouts/main.blade.php
+++ aprendendo-laravel/resources/views/layouts/main.blade.php
@@ -7,7 +7,7 @@
         <title>@yield('title')</title>
 
         <!-- Fonte fo google -->
-         <link href="https://fonts.googleapis.com/css2?family=Roboto" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Roboto" rel="stylesheet">
 
         <!-- CSS Boootstrap -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
@@ -17,26 +17,43 @@
         <script src="/js/scripts.js"></script>
     </head>
     <body>
-    <header>
-    <nav class="navbar navbar-expand-lg navbar-light">
-        <div class="collapse navbar-collapse" id="navbar">
-            <a href="/" class="navbar-brend">
-             <img src="/img/projeto14.jpg" alt="ForgeOne Events">   
-            </a>
-            <ul class="navbar-nav">
+      <header>
+        <nav class="navbar navbar-expand-lg navbar-light">
+            <div class="collapse navbar-collapse" id="navbar">
+                <a href="/" class="navbar-brend">
+                    <img src="/img/projeto14.jpg" alt="ForgeOne Events">   
+                </a>
+                <ul class="navbar-nav">
+             @auth
              <li class="nav-item">
-                <a href="/" class="nav-link">Cadastrar</a>
+                <a href="/dashboard" class="nav-link">Meus eventos</a>
              </li> 
-                          <li class="nav-item">
-                <a href="/" class="nav-link">Entar</a>
-             </li>
+             <li class="nav-item">
+                <form action="/logout" method="POST">
+                  @csrf
+                  <a href="/logout" 
+                    class="nav-link" 
+                    onclick="event.preventDefault();
+                    this.closest('form').submit();">
+                    Sair
+                   </a>
+                </form>
+             </li> 
+             @endauth       
+            @guest          
+             <li class="nav-item">
+                <a href="/login" class="nav-link">Entrar</a>
+             </li>           
+            <li class="nav-item">
+                <a href="/register" class="nav-link">Cadastrar</a>
+            </li>
+            @endguest
                           <li class="nav-item">
                 <a href="/" class="nav-link">Evento</a>
              </li>
                           <li class="nav-item">
                 <a href="/events\create" class="nav-link">Criar Evento</a>
              </li> 
-               
             </ul>
         </div>
     </nav>
